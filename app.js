abe7d0906c5c9340d5ebaf55d1d28ce7563f34ca
Nord <arinatdd@gmail.com>
2021-05-17 11:13:50 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+const router = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log('Сервер запущен');
+});
---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+module.exports = {
+  JWT_SECRET,
+};
